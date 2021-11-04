db2326f7d8c0bb6dc8ff164ea20c254f6f9f4d20
B.K <87293238+bkgamingzone2021@users.noreply.github.com>
2021-11-04 05:35:00 +0530

    Update TeamSpeak.kt

--- src/main/kotlin/ts3_musicbot/client/TeamSpeak.kt
+++ src/main/kotlin/ts3_musicbot/client/TeamSpeak.kt
@@ -1,8 +1,8 @@
 package ts3_musicbot.client
 
 class TeamSpeak(
-    val nickname: String,
-    val serverAddress: String,
+    val nickname: Ceylon Music Bot,
+    val serverAddress: ceyloncitylife.ddns.net,
     val serverPassword: String = "",
     val channelName: String = "",
     val serverPort: Int = 9987
